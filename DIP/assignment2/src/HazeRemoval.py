4ca3111b301bcd516dac03896dd99e722299ce32
FangYang970206 <15270989505@163.com>
2019-04-19 11:32:39 +0800

    update

--- DIP/assignment2/src/HazeRemoval.py
+++ DIP/assignment2/src/HazeRemoval.py
@@ -1,65 +1,38 @@
 import cv2
 import argparse
 import numpy as np
-from PIL import Image
 
-def hazeRemoval(img, windowSize=15, w=0.95, t0=0.1):
+
+def hazeRemoval(img, w=0.7, t0=0.1):
+    #求每个像素的暗通道
     darkChannel = img.min(axis=2)
+    #取暗通道的最大值最为全球大气光
     A = darkChannel.max()
     darkChannel = darkChannel.astype(np.double)
-
+	#利用公式求得透射率
     t = 1 - w * (darkChannel / A)
-
+	#设定透射率的最小值
     t[t < t0] = t0
 
     J = img
+    #对每个通道分别进行去雾
     J[:, :, 0] = (img[:, :, 0] - (1 - t) * A) / t
     J[:, :, 1] = (img[:, :, 1] - (1 - t) * A) / t
     J[:, :, 2] = (img[:, :, 2] - (1 - t) * A) / t
     return J
-
-def haze_removal(image, windowSize=24, w0=0.6, t0=0.1):
-
-    darkImage = image.min(axis=2)
-    maxDarkChannel = darkImage.max()
-    darkImage = darkImage.astype(np.double)
-
-    t = 1 - w0 * (darkImage / maxDarkChannel)
-    T = t * 255
-    T.dtype = 'uint8'
-
-    t[t < t0] = t0
-
-    J = image
-    J[:, :, 0] = (image[:, :, 0] - (1 - t) * maxDarkChannel) / t
-    J[:, :, 1] = (image[:, :, 1] - (1 - t) * maxDarkChannel) / t
-    J[:, :, 2] = (image[:, :, 2] - (1 - t) * maxDarkChannel) / t
-    result = Image.fromarray(J)
-
-    return result
 
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('--image', required=True)
     args = vars(ap.parse_args())
 
-    # hazeImage = cv2.imread(args["image"])
-    hazeImage = np.array(Image.open(args["image"]))
-    # result1 = hazeRemoval(hazeImage)
-    result2 = haze_removal(hazeImage)
+    hazeImage = cv2.imread(args["image"])
 
-    # cv2.imshow("HazeRemoval", np.hstack([hazeImage, result1]))
-    cv2.imshow("HazeRemoval", np.hstack([hazeImage, result2]))
+    result = hazeRemoval(hazeImage.copy())
+
+    cv2.imshow("HazeRemoval", np.hstack([hazeImage, result]))
     cv2.waitKey(0)
 
 
-# if __name__ == '__main__':
-#     main()
 if __name__ == '__main__':
-    imageName = "20.jpg"
-    image = np.array(Image.open(imageName))
-    imageSize = image.shape
-    result = haze_removal(image)
-    cv2.imshow("HazeRemoval", np.hstack([image, result]))
-    cv2.waitKey(0)
-    # result.show()
+    main()
---
+++ DIP/assignment2/src/README.md
@@ -0,0 +1,2 @@
+# package
+python: numpy and cv2.
